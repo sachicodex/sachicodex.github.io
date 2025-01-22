06978166b23e325bdf96fb65a30373a363e59d63
sachicodex <hello.sachinthalakshan@gmail.com>
2025-01-22 18:15:53 +0530

    Add a Nav Menu

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ function scrollToSocial() {
   document.getElementById('socialD').scrollIntoView({ behavior: 'smooth' });
   document.getElementById('socialM').scrollIntoView({ behavior: 'smooth' });
 }
+
+function openNav() {
+  navBar = document.getElementById('nav')
+
+  navBar.style.display = 'block';
+}
+
+function closeNav() {
+  navBar = document.getElementById('nav')
+
+  navBar.style.display = 'none';
+}
