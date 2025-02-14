1903fc5834bc784136590090098e7cd507dff445
sachicodex <hello.sachinthalakshan@gmail.com>
2025-02-14 23:33:49 +0530

    Add Modern Header For Desktop Viewers

--- js/script.js
+++ js/script.js
@@ -24,3 +24,29 @@ window.onload = () => {
   setTimeout(showHeader, 5700);
 };
 
+// Header Animation
+
+document.addEventListener('DOMContentLoaded', function () {
+  const headerLis = document.querySelectorAll('header ul li');
+  const defaultActiveLi = document.querySelector('header ul li:nth-child(4)');
+  const header = document.querySelector('header');
+
+  let activeLi = defaultActiveLi;
+  activeLi.classList.add('active-item');
+
+  headerLis.forEach(li => {
+    li.addEventListener('mouseover', function () {
+      if (activeLi !== li) {
+        activeLi.classList.remove('active-item');
+      }
+      activeLi = li;
+      li.classList.add('active-item');
+    });
+
+    li.addEventListener('mouseout', function () {
+      activeLi.classList.remove('active-item');
+      defaultActiveLi.classList.add('active-item');
+      activeLi = defaultActiveLi;
+    });
+  });
+});
